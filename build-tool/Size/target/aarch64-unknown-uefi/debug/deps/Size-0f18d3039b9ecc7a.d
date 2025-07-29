a1c91919e5bc22f58415c634f34bd48a1bfe802b
Marissa Ovenshire <ovenshirem@gmail.com>
2025-07-29 12:44:13 -0700

    remove junk I meant to ignore from tgt build artifacts

---
+++ build-tool/Size/target/aarch64-unknown-uefi/debug/deps/Size-0f18d3039b9ecc7a.d
@@ -0,0 +1,6 @@
+/home/movenshire/blueberry/build-tool/Size/target/aarch64-unknown-uefi/debug/deps/Size-0f18d3039b9ecc7a.d: src/main.rs src/uart_debug.rs
+
+/home/movenshire/blueberry/build-tool/Size/target/aarch64-unknown-uefi/debug/deps/Size-0f18d3039b9ecc7a.efi: src/main.rs src/uart_debug.rs
+
+src/main.rs:
+src/uart_debug.rs:
--- build-tool/Size/target/aarch64-unknown-uefi/debug/deps/Size-1e8b75f02fe05ef0.d
+++
@@ -1,6 +0,0 @@
-/home/movenshire/blueberry/build-tool/Size/target/aarch64-unknown-uefi/debug/deps/Size-1e8b75f02fe05ef0.d: src/main.rs src/uart_debug.rs
-
-/home/movenshire/blueberry/build-tool/Size/target/aarch64-unknown-uefi/debug/deps/Size-1e8b75f02fe05ef0.efi: src/main.rs src/uart_debug.rs
-
-src/main.rs:
-src/uart_debug.rs:
