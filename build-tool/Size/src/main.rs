843ed84c46e9249a31e856892448a90db4098c4d
Marissa Ovenshire <ovenshirem@gmail.com>
2025-07-29 11:59:56 -0700

    Yes alloc from MU allocator base

--- build-tool/Size/src/main.rs
+++ build-tool/Size/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![allow(non_snake_case)]
 
+extern crate alloc;
 
 use core::panic::PanicInfo;
 use r_efi::efi::Status;
@@ -11,13 +12,13 @@ mod uart_debug;
 #[no_mangle]
 pub extern "efiapi" fn efi_main(
     _image_handle: *const core::ffi::c_void,
-    _system_table: *const SystemTable,
+    system_table: *const SystemTable,
 ) -> Status {
     unsafe {
         uart_debug::uart_init();
         uart_debug::log("ENTER: efi_main entry point");
-        //let bs = (*system_table).boot_services;
-        //rust_boot_services_allocator_dxe::GLOBAL_ALLOCATOR.init(bs);
+        let bs = (*system_table).boot_services;
+        rust_boot_services_allocator_dxe::GLOBAL_ALLOCATOR.init(bs);
 
         // Install Protocol
 
