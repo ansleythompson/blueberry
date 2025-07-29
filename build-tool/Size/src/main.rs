0dcbed2a386c3c81ee720776ff4bd9d9ee8dece4
Marissa Ovenshire <ovenshirem@gmail.com>
2025-07-29 12:07:50 -0700

    No dbg mod

--- build-tool/Size/src/main.rs
+++ build-tool/Size/src/main.rs
@@ -5,9 +5,18 @@
 extern crate alloc;
 
 use core::panic::PanicInfo;
-use r_efi::efi::Status;
+use r_efi::efi::{Guid, Status};
 use r_efi::system::SystemTable;
-mod uart_debug;
+//mod uart_debug;
+
+pub const G_USB_EXT_PROTOCOL_GUID: Guid = Guid::from_fields(
+    0x3a7f1e32,
+    0xd5a5,
+    0x498a,
+    0x8c,
+    0x2c,
+    &[0x19, 0x27, 0x11, 0x76, 0x18, 0x64],
+);
 
 #[no_mangle]
 pub extern "efiapi" fn efi_main(
@@ -15,8 +24,8 @@ pub extern "efiapi" fn efi_main(
     system_table: *const SystemTable,
 ) -> Status {
     unsafe {
-        uart_debug::uart_init();
-        uart_debug::log("ENTER: efi_main entry point");
+        //uart_debug::uart_init();
+        //uart_debug::log("ENTER: efi_main entry point");
         let bs = (*system_table).boot_services;
         rust_boot_services_allocator_dxe::GLOBAL_ALLOCATOR.init(bs);
 
@@ -24,7 +33,17 @@ pub extern "efiapi" fn efi_main(
 
 
         // Locate Protocol
-
+        /* 
+        let status = ((*bs).locate_protocol)(
+            &G_USB_EXT_PROTOCOL_GUID as *const _,
+            null_mut(),
+            &mut protocol as *mut _,
+        );
+        if status != Status::SUCCESS {
+            uart_debug::log("Failed to locate USB protocol.");
+            return Status::NOT_FOUND;
+        }
+*/
         // Create event
 
     }
